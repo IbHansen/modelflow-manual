460e0264c5002429fee944eedae3797b67440c33
IbHansen <43575358+IbHansen@users.noreply.github.com>
2023-01-18 09:26:35 +0100

    back from reorganisation

--- model_paaper/build.py
+++ model_paaper/build.py
@@ -26,7 +26,7 @@ buildhtml = buildloc / 'html'
 (destination := Path(r'C:/modelbook/IbHansen.github.io/mf')).mkdir(parents=True, exist_ok=True)
 fileloc = str((buildhtml / 'index.html').absolute())
 
-# print(fileloc)
+print(f'{fileloc=}\n{destination=}')
 # breakpoint()
 xx0 = run(f'jb build {bookdir}/ {doall}')
 # wb.open(rf'file://C:\wb new\Modelflow\working_paper\{bookdir}\_build\html\index.html', new=2)
