79da64338280ee4c0edabdcbb2ea40c26acacbe9
IbHansen <43575358+IbHansen@users.noreply.github.com>
2023-04-25 16:12:33 +0200

    Update build.py

--- papers/build.py
+++ papers/build.py
@@ -12,7 +12,7 @@ look at the cheatcheet.
 from subprocess import run
 import webbrowser as wb
 from pathlib import Path
-from shutil import copy, copytree
+import yaml 
  
 import sys
 print(sys.argv)
@@ -34,7 +34,10 @@ fileloc = str((buildhtml / 'index.html').absolute())
 
 #print(f'{fileloc=}\n{destination=}')
 print(f'{fileloc=}\n') #dropped destination
-# breakpoint()
+
+
+breakpoint()
+assert 1==2
 xx0 = run(f'jb build {bookdir}/ {doall}')
 # wb.open(rf'file://C:\wb new\Modelflow\working_paper\{bookdir}\_build\html\index.html', new=2)
 wb.open(rf'file://{fileloc}', new=2)
@@ -66,11 +69,23 @@ for dir in sorted(Path(f'{bookdir}/_build/jupyter_execute').glob('**')):
             # print(f'Not copied{picture}')
      
 if 'latex-pdf' in options: 
-     xx0 = run(f'jb build {bookdir}/ --builder=latex')
-     xx0 = run(f'cd {bookdir}/_build/latex/')
-     xx0 = run(f'latexmk -pdf -dvi- -ps- -f MFModinModelflow.tex')
+    configloc = Path(f'{bookdir}/_config.yml')
+    with open(configloc,'r') as f: 
+        yaml_dict= yaml.load(f.read(),Loader=yaml.SafeLoader)
+    try:
+        texfile= yaml_dict['latex']['latex_documents']['targetname']  
+    except: 
+        texfile = 'book.tex'
+        
+    xx0 = run(f'jb build {bookdir}/ --builder=latex')
+    xx0 = run(f'latexmk -pdf -dvi- -ps- -f {texfile}',cwd=f'{bookdir}/_build/latex/')
+    
+    pdffile = Path(f'{bookdir}/_build/latex/'+(texfile.split('.')[0]+'.pdf')).absolute()
+    
+    
      
 if 'copy' in options:
+    destination = Path(fr'C:/modelbook/IbHansen.github.io/{bookdir}')
     copytree(buildhtml,destination,dirs_exist_ok=True )
      
     
