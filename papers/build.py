60dab86a0edc152dee8ed123e4d2d8230120f135
AndBurns <home@a-burns.com>
2023-09-05 17:23:48 -0400

    Add index functionality
    Works on the html version but weirdly not the PDF

--- papers/build.py
+++ papers/build.py
@@ -67,10 +67,11 @@ for dir in sorted(Path(f'{bookdir}/_build/jupyter_execute').glob('**')):
             ...
             # print(f'Not copied{picture}')
      
-if 'latex-pdf' in options: 
+if 'latex-pdf' or 'pdf-latex' in options: 
      xx0 = run(f'jb build {bookdir}/ --builder=latex')     
      xx0 = run(f'jb build {bookdir}/ --builder=latex')
      xx0 = run('latexmk -pdf -dvi- -ps- -f MFModinModelflow.tex',cwd = f'{bookdir}/_build/latex/')
+     print(f'PDF generated: see {bookdir}/_build/latex/')
 
      
 if 'copy' in options:
