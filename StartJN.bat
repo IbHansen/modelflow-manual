e80958d2ca3969c7fabeba096ed9a7ab3cda736d
AndBurns <home@a-burns.com>
2023-09-24 17:59:05 -0400

    Some additional Unnada changes and revise attribution to say decomposition

---
+++ StartJN.bat
@@ -0,0 +1,4 @@
+
+cd c:\mflow\aug23\papers
+conda activate modelflow
+C:\ProgramData\Anaconda3\python.exe C:\ProgramData\Anaconda3\cwp.py C:\Users\wb268970\.conda\envs\modelflow C:\Users\wb268970\.conda\envs\modelflow\python.exe C:\Users\wb268970\.conda\envs\modelflow\Scripts\jupyter-notebook-script.py "%USERPROFILE%/"
